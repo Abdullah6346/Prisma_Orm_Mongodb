3fa1fc87fe28172e64a2b07c4eec3bc35338b579
muhammad abdullah <muhmmadabdullah6346@gmail.com>
2024-07-30 13:39:08 +0500

    feat: I added Replica et Locally
    I have used three instances to create data

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from "express";
 const app = express();
+import { PrismaClient } from "@prisma/client";
 const port = 3000;
+const prisma = new PrismaClient();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  await prisma.$connect();
+  const cr_usr_dat = await prisma.user.create({ data: req.body });
+  res.send(cr_usr_dat);
 });
 
 app.listen(port, () => {
